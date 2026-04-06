b580b7f81b5b223261600d4ab51477b24adcd300
Cesar <cesarscfilho@gmail.com>
2026-04-06 09:21:56 -0300

    fix: corrigir interceptor axios que sobrescrevia headers customizados ao estar logado

--- apps/agendar-web/src/lib/api-client.ts
+++ apps/agendar-web/src/lib/api-client.ts
@@ -10,11 +10,7 @@ api.interceptors.request.use(config => {
   const token = getToken()
 
   if (token) {
-    // @ts-expect-error
-    config.headers = {
-      ...config.headers,
-      Authorization: `Bearer ${token}`,
-    }
+    config.headers.Authorization = `Bearer ${token}`
   }
 
   return config
